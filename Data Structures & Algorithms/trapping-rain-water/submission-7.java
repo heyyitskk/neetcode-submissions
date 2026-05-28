a9140c6b1129d20b4dca433f8d5fd3de49416fcb
Kunal Kittur <kunalkittur23@gmail.com>
2026-05-28 13:25:43 +0530

    Add: trapping-rain-water - submission-7

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-7.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int trap(int[] height) {
+        int i = 0, j = height.length - 1;
+        int water = 0, leftMax = height[i], rightMax = height[j];
+        while(i < j){
+            if(leftMax < rightMax){
+                i++;
+                leftMax = Math.max(leftMax, height[i]);
+                water += leftMax - height[i];
+            }
+            else{
+                j--;
+                rightMax = Math.max(rightMax, height[j]);
+                water += rightMax - height[j];
+            }
+        }
+        return water;
+    }
+}
